d28fdf1ea4628a5e37d781cbbe45979f78f1dc0b
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2023-01-24 10:52:06 -0800

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ *bubble_sort - sorts an array of integers in ascending order.
+ *@array : an array of integers
+ *@size : size of an array
+ */
+
+void bubble_sort(int *array, size_t size);
+
+{
+	int i, j;
+	int temp;
+
+	for (i = 0; i < size - 1; i++)
+
+	{
+		for (j = 0; size - 1 - i; j++)
+
+		{
+			if (array[j] > array[j + 1])
+
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1 = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
