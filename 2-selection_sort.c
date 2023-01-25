8bc71dd75ec989c9df88487f88774756d445ead7
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2023-01-25 06:03:23 -0800

    selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+#include <stdio.h>
+
+
+/**
+ * selection_sort- sorts integers according to selection algorithm
+ * @array: array of integers
+ * @size : size of an array
+ */
+
+void selection_sort(int *array, size_t size)
+
+{
+	unsigned int i;
+	unsigned int j;
+	unsigned int min_idx;
+	int temp;
+
+	for (i = 0; i < size; i++)
+
+	{
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+				min_idx = j
+		}
+
+			temp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = temp;
+
+			if (i != min_idx)
+				print_array(array, size);
