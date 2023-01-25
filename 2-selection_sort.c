d671e7727c82d7758fe6124a05c1ac648ab069ba
Pelo101 <110992136+Pelo101@users.noreply.github.com>
2023-01-25 08:39:41 -0800

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -19,12 +19,12 @@ void selection_sort(int *array, size_t size)
 	if (size < 2)
 		return;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 
 	{
 		min_idx = i;
 
-		for (j = i + 1; j < size - i - 1; j++)
+		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_idx])
 				min_idx = j;
